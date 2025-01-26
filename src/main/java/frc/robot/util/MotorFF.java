734b91aeeccc1d19102717bed08e8e794aa19b16
Andrew Chen <bobopop787@gmail.com>
2025-01-26 12:50:11 -0500

    edit clarification

--- src/main/java/frc/robot/util/MotorFF.java
+++ src/main/java/frc/robot/util/MotorFF.java
@@ -2,7 +2,7 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-// Edited by Team 178 for adjustable constants, based on edu.wpi.first.math.controller.SimpleMotorFeedforward
+// Edited by Team 178 for adjustable constants, based on edu.wpi.first.math.controller.ArmFeedforward
 
 package frc.robot.util;
 
